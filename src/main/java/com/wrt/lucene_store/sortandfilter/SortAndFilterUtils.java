5caa3b082c25c52b23eb92bf26ac4065761aa014
王瑞涛 <wangruitao@midaigroup.com>
2017-01-18 14:36:30 +0800

    排序

--- src/main/java/com/wrt/lucene_store/sortandfilter/SortAndFilterUtils.java
+++ src/main/java/com/wrt/lucene_store/sortandfilter/SortAndFilterUtils.java
@@ -43,4 +43,27 @@ public class SortAndFilterUtils {
 			e.printStackTrace();
 		}
 	}
+	
+	public void fSearch() {
+		reader = PageIndexUtils.getDirectoryReader();
+		IndexSearcher search = new IndexSearcher(reader);
+		QueryParser parser = new QueryParser("name", new StandardAnalyzer());
+		try {
+			Query query = parser.parse("[a* TO e*] AND suffix:ini");
+//			TopFieldDocs docs = search.search(query, 10, Sort.INDEXORDER);
+			SortField sfield = new SortField("size", Type.INT, true);
+			Sort sort = new Sort(sfield);
+			TopFieldDocs docs = search.search(query, 450, sort);
+			ScoreDoc[] sds = docs.scoreDocs;
+			System.out.println("查询总数： " + docs.totalHits);
+			Document doc = null;
+			for (ScoreDoc sd : sds) {
+				doc = search.doc(sd.doc);
+				System.out.println("docId:" + sd.doc + " name: " + doc.get("name") + " suffix:" + doc.get("suffix") + " size:" + doc.get("filesize") + " content:"
+						+ doc.get("content") + " path:" + doc.get("path") + " filedate:" + doc.get("filedate"));
+			}
+		} catch (ParseException | IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
